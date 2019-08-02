3f4a05a09d8f0473c1bf8d3389ae8595e11d3093
Jessica Anastasio <jessicaanastasio39@gmail.com>
2019-08-01 22:04:46 -0400

    set default headshot

--- src/App.js
+++ src/App.js
@@ -1,42 +1,44 @@
-import React, { useState, useEffect, useRef } from 'react';
-import resume from './resume.pdf';
-import './App.css';
-
+import React, { useState, useEffect, useRef } from "react";
+import myFace from "./images/myface/myface.jpg";
+import resume from "./resume.pdf";
+import "./App.css";
 
 const App = () => {
-
-  const importAllFaceImgs = (r) => {
+  const importAllFaceImgs = r => {
     let images = [];
-    r.keys().map((item) => { images.push(r(item)); });
+    r.keys().map(item => {
+      images.push(r(item));
+    });
     return images;
-  }
+  };
 
-  const faceImgs = importAllFaceImgs(require.context('./images/myface', false, /\.(png|jpe?g|svg)$/));
-  const colors = ['#24d05a', '#eb4888', '#10a2f5', '#e9bc3f'];
+  const faceImgs = importAllFaceImgs(
+    require.context("./images/myface", false, /\.(png|jpe?g|svg)$/)
+  );
+  const colors = ["#24d05a", "#eb4888", "#10a2f5", "#e9bc3f"];
 
   // image component that will update w random image URL
   const RotatingPhoto = () => {
-    const firstImageURL = getRandomImageURL(faceImgs);
+    console.log(faceImgs)
+    const firstImageURL = myFace; // always set this headshot first
     const [imageURL, setImageURL] = useState(firstImageURL);
-    
+
     useInterval(() => {
       let newImageURL = getRandomImageURL(faceImgs);
       setImageURL(newImageURL);
     }, 3000);
 
-    return (
-      <img src={imageURL} alt="Jess Anastasio" />
-    );
-  }
-  
-  // given array of images, choose random index
-  const getRandomImageURL = (faceImgs) => {
-    return faceImgs[Math.floor(Math.random() * faceImgs.length)];    
-  }
+    return <img src={imageURL} alt="Jess Anastasio" />;
+  };
 
-  const getRandomColor = (colors) => {
+  // given array of images, choose random index
+  const getRandomImageURL = faceImgs => {
+    return faceImgs[Math.floor(Math.random() * faceImgs.length)];
+  };
+
+  const getRandomColor = colors => {
     return colors[Math.floor(Math.random() * colors.length)];
-  }
+  };
 
   const Links = () => {
     const [linkColor, setLinkColor] = useState(colors[0]);
@@ -50,14 +52,24 @@ const App = () => {
 
     return (
       <div className="links">
-        <a style={linkStyle} href="http://twitter.com/jessanastasio">twitter</a>
-        <a style={linkStyle} href="http://linkedin.com/in/jessicaanastasio">linkedin</a>
-        <a style={linkStyle} href="http://github.com/jessanastasio">github</a>
-        <a style={linkStyle} href={resume}>resume</a>
-        <a style={linkStyle} href="mailto:jessicaanastasio39@gmail.com">email</a>
+        <a style={linkStyle} href="http://twitter.com/jessanastasio">
+          twitter
+        </a>
+        <a style={linkStyle} href="http://linkedin.com/in/jessicaanastasio">
+          linkedin
+        </a>
+        <a style={linkStyle} href="http://github.com/jessanastasio">
+          github
+        </a>
+        <a style={linkStyle} href={resume}>
+          resume
+        </a>
+        <a style={linkStyle} href="mailto:jessicaanastasio39@gmail.com">
+          email
+        </a>
       </div>
     );
-  }
+  };
 
   const useInterval = (callback, delay) => {
     const savedCallback = useRef();
@@ -77,21 +89,41 @@ const App = () => {
         return () => clearInterval(id);
       }
     }, [delay]);
-  }
+  };
 
   return (
     <div className="App">
       <div className="header">
         <div className="img-container">
-          <RotatingPhoto/>
+          <RotatingPhoto />
         </div>
         <div className="about-me">
           <h2>Jess Anastasio</h2>
           <ol>
-            <li><span className="about-emoji" role="img" aria-label="liberty">🗽</span> software engineer in nyc</li>
-            <li><span className="about-emoji" role="img" aria-label="heart">💕</span> loves javascript</li>
-            <li><span className="about-emoji" role="img" aria-label="disk">💾</span> huge tech nerd</li>
-            <li><span className="about-emoji" role="img" aria-label="mic">🎤</span> karaoke fan</li>
+            <li>
+              <span className="about-emoji" role="img" aria-label="liberty">
+                🗽
+              </span>{" "}
+              software engineer in nyc
+            </li>
+            <li>
+              <span className="about-emoji" role="img" aria-label="heart">
+                💕
+              </span>{" "}
+              loves javascript
+            </li>
+            <li>
+              <span className="about-emoji" role="img" aria-label="disk">
+                💾
+              </span>{" "}
+              huge tech nerd
+            </li>
+            <li>
+              <span className="about-emoji" role="img" aria-label="mic">
+                🎤
+              </span>{" "}
+              karaoke fan
+            </li>
           </ol>
         </div>
       </div>
@@ -100,25 +132,62 @@ const App = () => {
 
       <div className="more">
         <div className="blurb">
-          Hi, I’m Jess! I'm a software engineer at <a href="https://buzzfeed.com">BuzzFeed</a> where I work on full-stack web development. I also help drive diversity and inclusion efforts by leading BuzzFeed’s LGBTQ employee resource group and volunteering at conferences like Grace Hopper and Lesbians who Tech.
+          Hi, I’m Jess! I'm a software engineer at{" "}
+          <a href="https://buzzfeed.com">BuzzFeed</a> where I work on full-stack
+          web development. I also help drive diversity and inclusion efforts by
+          leading BuzzFeed’s LGBTQ employee resource group and volunteering at
+          conferences like Grace Hopper and Lesbians who Tech.
         </div>
         <div>
-          My goal as an engineer is to show craftsmanship, courtesy and empathy in my work and interactions with others.
+          My goal as an engineer is to show craftsmanship, courtesy and empathy
+          in my work and interactions with others.
         </div>
       </div>
 
       <div className="fun-facts">
-        <h3>fun facts:</h3>           
+        <h3>fun facts:</h3>
         <ol>
-          <li><span role="img" aria-label="star">👩🏼‍🔧</span> I used to work in the computer department at Best Buy and built my own computers</li>
-          <li><span role="img" aria-label="star">⚡️</span> I spoke at <a className="amp" href="https://www.youtube.com/watch?v=HsCq5XIprXY&feature=youtu.be&t=224">Google's AMP Conf 2018</a> and contributed to AMP open source spec</li>
-          <li><span role="img" aria-label="star">🔥</span> I have a great love for <a className="twitter" href="https://twitter.com/jessanastasio">javascript memes</a> (actually all memes)</li>
-          <li><span role="img" aria-label="star">☕️</span> You can probably find me skateboarding around the city or drinking coffee </li>
+          <li>
+            <span role="img" aria-label="star">
+              👩🏼‍🔧
+            </span>{" "}
+            I used to work in the computer department at Best Buy and built my
+            own computers
+          </li>
+          <li>
+            <span role="img" aria-label="star">
+              ⚡️
+            </span>{" "}
+            I spoke at{" "}
+            <a
+              className="amp"
+              href="https://www.youtube.com/watch?v=HsCq5XIprXY&feature=youtu.be&t=224"
+            >
+              Google's AMP Conf 2018
+            </a>{" "}
+            and contributed to AMP open source spec
+          </li>
+          <li>
+            <span role="img" aria-label="star">
+              🔥
+            </span>{" "}
+            I have a great love for{" "}
+            <a className="twitter" href="https://twitter.com/jessanastasio">
+              javascript memes
+            </a>{" "}
+            (actually all memes)
+          </li>
+          <li>
+            <span role="img" aria-label="star">
+              ☕️
+            </span>{" "}
+            You can probably find me skateboarding around the city or drinking
+            coffee{" "}
+          </li>
         </ol>
       </div>
-      
     </div>
   );
-}
+};
 
 export default App;
